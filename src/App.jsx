f76e96e176b9e32907eb61f7c2359b116b03ba7c
TahsinZahir <tahsindbg0142@gmail.com>
2024-11-28 23:21:10 +0530

    Added navbar and banner

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import './App.css'
+import Banner from './components/Banner/Banner'
+import Navbar from './components/Navbar/Navbar'
+import CoinTable from './components/CoinTable/CoinTable'
 function App(){
 return (
-  <>Hello</>
+  <>
+  <Navbar/>
+  <Banner/>
+  <CoinTable/>
+  </>
 )
 }
 export default App
