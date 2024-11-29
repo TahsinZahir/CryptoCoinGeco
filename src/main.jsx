770893b3eb57d381ab3d25e49416eb6bc6a9fadb
TahsinZahir <tahsindbg0142@gmail.com>
2024-11-29 11:46:19 +0530

    Added react query config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const queryClient=new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<QueryClientProvider client={queryClient}>
+  <App/>
+</QueryClientProvider>
 )
+
