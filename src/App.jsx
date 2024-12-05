41908c200b20e12e6ccda2a8d1d02e8d2e608f6a
TahsinZahir <tahsindbg0142@gmail.com>
2024-12-06 00:39:16 +0530

    Added layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,8 @@
-//import { useState } from 'react'
 import './App.css'
 import Routing from './components/Routing/Routing'
-// import Home from './pages/Home'
-// import { CurrencyContext } from './context/CurrencyContext';
 function App(){
-//const [currency,setCurrency]=useState('usd')
 return (
   <>
-{/* <CurrencyContext.Provider value={ { currency, setCurrency }}>
-<Home/>
-</CurrencyContext.Provider> */}
 <Routing/>
   </>
 )
